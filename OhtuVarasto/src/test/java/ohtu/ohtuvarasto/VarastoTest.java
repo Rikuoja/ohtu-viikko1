80bac63ad01ee28e2c027a160541fea809eda54c
Riku Oja <riku.oja@kapsi.fi>
2015-03-10 17:21:01 +0200

    Break test

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -75,7 +75,7 @@ public class VarastoTest {
     public void ottaminenLisääTilaa() {
         varasto.lisaaVarastoon(8);
 
-        varasto.otaVarastosta(2);
+        varasto.otaVaraststa(2);
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
